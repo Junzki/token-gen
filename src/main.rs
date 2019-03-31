044ca4c1bb7baa9f05b6bb38c63fd26374649d35
Andrew Junzki <andrew@junzki.me>
2019-03-31 11:06:42 +0800

    WIP: Read config from YAML.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate argparse;
 extern crate jsonwebtoken as jwt;
-extern crate toml;
-#[macro_use]
-extern crate serde_derive;
+extern crate yaml_rust;
+#[macro_use] extern crate serde_derive;
 
 use std::fs::File;
 use std::io::prelude::*;
 use argparse::{ArgumentParser, Store};
 use jwt::{encode, Header};
+use yaml_rust::YamlLoader;
+
 mod models;
 
 
@@ -28,10 +29,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     f.read_to_string(&mut spec)?;
 
-    let spec: models::Spec = toml::from_str(&spec).unwrap();
+    // let spec = YamlLoader::load_from_str(&spec).unwrap();
+    // println!("{:?}", &(spec[0]));
 
-    let token = encode(&Header::default(), &(spec.payload.unwrap()), spec.generic.secret.as_ref());
-    println!("Bearer {}", token.unwrap());
+    // let token = encode(&Header::default(), &(spec.payload.unwrap()), spec.generic.secret.as_ref());
+    // println!("Bearer {}", token.unwrap());
+    let config = models::config_from_string(&spec)?;
+    println!("{:?}", &config.default_context);
 
     Ok(())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,24 +1,62 @@
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct Claims {
-//     pub uid: String,
-//     pub scopes: Vec<String>
-// }
+use std::error::Error;
+use yaml_rust::{Yaml, YamlLoader};
 
-#[derive(Deserialize)]
-pub struct Spec {
-    pub generic: Generic,
-    pub payload: Option<Payload>
-}
-
-#[derive(Deserialize)]
-pub struct Generic {
-    pub project: String,
-    pub algorithm: String,
-    pub secret: String
-}
 
 #[derive(Serialize, Deserialize)]
-pub struct Payload {
+pub struct Fields {
     pub uid: String,
     pub scopes: Vec<String>
+}
+
+pub struct Context {
+    pub name: String,
+    pub algorithm: String,
+    pub secret: String,
+    pub fields: Option<Fields>
+}
+
+pub struct Config {
+    pub contexts: Vec<Context>,
+    pub default_context: String
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            default_context: String::new(),
+            contexts: Vec::new()
+        }
+    }
+}
+
+impl Default for Context {
+    fn default() -> Context {
+        Context {
+            name: String::new(),
+            algorithm: String::from("HS256"),
+            secret: String::new(),
+            fields: None
+        }
+    }
+}
+
+pub fn config_from_string(config_str: &String) -> Result<Config, Box<dyn Error>> {
+    let spec = YamlLoader::load_from_str(config_str)?;
+    let spec = &spec[0];
+
+    let mut config = Config { ..Default::default() };
+
+    let default_context = spec["default_context"].as_str().unwrap();
+    config.default_context = default_context.to_string();
+
+    let contexts = spec["contexts"].as_vec().unwrap();
+    for original in contexts {
+        let mut cxt = Context { ..Default::default() };
+        cxt.name = original["name"].as_str().unwrap().to_string();
+
+        let algorithm = original["algorithm"].as_str().unwrap().to_string();
+    }
+
+
+    Ok(config)
 }
